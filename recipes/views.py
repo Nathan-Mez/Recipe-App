700becf590d77fe0353b4fd2020a1c2dd98edc72
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-14 17:45:46 +0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# function to return home.html template
+def welcome(request):
+    return render(request, 'recipes/recipes_home.html')
