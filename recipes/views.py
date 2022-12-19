12dac591c8b8735c64652a59f2b0d114072bdd47
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-19 19:04:31 +0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView                  #to display lists and details
+from .models import Recipe                                             #to access Recipe model
 
 # Create your views here.
 
 # function to return home.html template
 def welcome(request):
     return render(request, 'recipes/recipes_home.html')
+
+class RecipeListView(ListView):                                           #class-based view
+   model = Recipe                                                         #specify model
+   template_name = 'recipes/main.html'                                    #specify template
+
+class RecipeDetailView(DetailView):                                       #class-based view
+    model = Recipe                                                        #specify model
+    template_name = 'recipes/detail.html'                                 #specify template         
