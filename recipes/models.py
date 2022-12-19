ddc1a516abc9a2a4dc4b5d2ea5d832e4ce7cd126
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-19 19:02:21 +0300

    Update models.py

--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 class Recipe (models.Model):
     name = models.CharField(max_length = 120)
     cooking_time = models.FloatField(help_text= 'In minutes')
     ingredients = models.CharField(max_length = 350)
-    small_description = models.TextField()
+    description = models.TextField()
+    pic = models.ImageField(upload_to='recipes', default='no_image.svg')
+
+    # calculate difficulty of recipe using cooking time and number of ingredients
+    def calculate_difficulty(self):
+        if self.cooking_time < 10 and len(self.ingredients)< 4:
+            difficulty = 'Easy'
+        elif self.cooking_time < 10 and len(self.ingredients) >= 4: 
+            difficulty = 'Medium'
+        elif self.cooking_time >= 10 and len(self.ingredients) < 4:    
+            difficulty = 'Intermediate'
+        elif self.cooking_time >= 10 and len(self.ingredients) >= 4: 
+            difficulty = 'Hard'  
+        return difficulty    
+
+    def get_absolute_url(self):
+       return reverse ('recipes:detail', kwargs={'pk': self.pk})     
 
     def __str__(self):
         return str(self.name)
