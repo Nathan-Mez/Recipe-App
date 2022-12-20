8882777888b2c6164dae636b763d35c086246e5e
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-20 14:26:14 +0300

    Add static ROOT directory and filename

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',  
     # recipe project related apps,
     'recipes'
 ]
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
 
