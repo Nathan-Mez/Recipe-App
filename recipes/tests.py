441abce27610e18181e45d845ae9615af46031ef
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-21 10:56:53 +0300

    Add functionality test for calculate-difficulty & get absolute url

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,7 +6,7 @@ class RecipeModelTest(TestCase):
 
     def setUpTestData():
        # Set up non-modified objects used by all test methods
-       Recipe.objects.create(name = 'Tea', cooking_time = 5, ingredients = 'tea-leaves, water, sugar', small_description = 'Add tea leaves to boiling water, then add sugar')
+       Recipe.objects.create(name = 'Tea', cooking_time = 5, ingredients = 'tea-leaves, water, sugar', description = 'Add tea leaves to boiling water, then add sugar')
     
 
     def test_small_desciption(self):
@@ -24,6 +24,17 @@ class RecipeModelTest(TestCase):
     def test_cookingtime_helptext(self):
        recipe = Recipe.objects.get(id=1)
        recipe_cookingtime = recipe._meta.get_field('cooking_time').help_text
-       self.assertEqual(recipe_cookingtime, 'In minutes')      
+       self.assertEqual(recipe_cookingtime, 'In minutes')    
+
+
+    def test_get_absolute_url(self):
+       recipe = Recipe.objects.get(id=1)
+       #get_absolute_url() should load the URL /list/1                # TEST TO CHECK get_absolute_url() FUNCTIONALITY
+       self.assertEqual(recipe.get_absolute_url(), '/list/1')      
+
+
+    def test_difficulty_calculation(self):
+       recipe = Recipe.objects.get(id=1)                              # TEST TO CHECK calculate_difficulty() FUNCTIONALITY
+       self.assertEqual(recipe.calculate_difficulty(), 'Easy')  
 
 
