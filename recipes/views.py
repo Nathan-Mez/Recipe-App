4e54c7f88ae6d2c2fcce6ee10bc59ef7ac7037c2
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-23 15:25:33 +0300

    Protect ListView, DetailView via authentication

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView                  #to display lists and details
 from .models import Recipe                                             #to access Recipe model
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -8,10 +9,10 @@ from .models import Recipe                                             #to acces
 def welcome(request):
     return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(ListView):                                           #class-based view
+class RecipeListView(LoginRequiredMixin, ListView):                       #class-based 'protected' view
    model = Recipe                                                         #specify model
    template_name = 'recipes/main.html'                                    #specify template
 
-class RecipeDetailView(DetailView):                                       #class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):                   #class-based 'protected' view
     model = Recipe                                                        #specify model
     template_name = 'recipes/detail.html'                                 #specify template         
