96ebe3e1e95d113fc4a2ec733f4cee5f80b9be0d
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-12-23 15:22:50 +0300

    Add name to welcome function path

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ from .views import RecipeDetailView
 app_name = 'recipes'
 
 urlpatterns = [
-   path('', welcome),
+   path('', welcome, name='home'),
    path('list/', RecipeListView.as_view(), name='list'),
    path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
